7a188bd00eeecfbf810b7d2bf4362a87d7a152a9
Антон <lopatuxin.a@gmail.com>
2022-03-19 19:27:11 +0300

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -7,13 +7,19 @@ public class Calculator {
         int four = 4;
         int five = 5;
         int six = 6;
-        int onePlusTwo = one + two;
+        int result = one + two;
         int sixDivTwo = six / two;
         int fiveMinusTwo = five - two;
         int fourTimeTwo = four * two;
-        System.out.println(onePlusTwo);
+        System.out.println(result);
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
+        one = 10;
+        two = 11;
+        result = one + two;
+        result -= 10;
+        System.out.println(result);
+
     }
 }
